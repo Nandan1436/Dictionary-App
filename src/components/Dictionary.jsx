c58990cf24e0a7f494d059554a534bcfbe825e34
Nandan Bhowmick <nandanbhow123@gmail.com>
2025-03-21 01:42:33 +0600

    added word suggestion feature for input

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -4,12 +4,14 @@ const Dictionary = () => {
   const [word, setWord] = useState("")
   const [data, setData] = useState(null)
   const [error, setError] = useState("")
+  const [suggestions, setSuggestions] = useState([])
 
-  const fetchWord =async()=>{
-    if (!word) return
+  const fetchWord =async(query)=>{
+    const searchWord = query || word
+    if (!searchWord) return
 
     try{
-      const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`)
       
       if(!res.ok){
         throw new Error(`Word not found!`)
@@ -23,8 +25,21 @@ const Dictionary = () => {
       setData(null)
     }
 
+  }
 
-
+  const fetchSuggestion = async(input)=>{
+    if(!input){
+      setSuggestions([])
+      return
+    }
+    try{
+      const res = await fetch(`https://api.datamuse.com/sug?s=${input}`)
+      const result = await res.json()
+      setSuggestions(result.slice(0,5).map(s=>s.word))
+    }
+    catch(error){
+      console.error("Error fetching suggestions:", error);
+    }
   }
 
   return (
@@ -32,19 +47,44 @@ const Dictionary = () => {
         <h1>Dictionary App</h1>
 
         <div className='children'>
-          <input 
-            type="text" 
-            placeholder='Enter word'
-            value={word}
-            onChange={(e)=>setWord(e.target.value)}
-            onKeyDown={(e)=>{
-              if(e.key==='Enter'){
-                fetchWord()
-              }
-            }}
-          />
-          <button onClick={fetchWord}>Enter</button>
-
+          <div className='input-container'>
+            <input 
+              type="text" 
+              placeholder='Enter word'
+              value={word}
+              onChange={(e)=>{
+                setWord(e.target.value)
+                fetchSuggestion(e.target.value)
+              }}
+              onKeyDown={(e)=>{
+                if(e.key==='Enter'){
+                  fetchWord()
+                  setSuggestions([])
+                }
+              }}
+            />
+            <button onClick={() => {
+              fetchWord(word)
+              setSuggestions([])
+              }}>Enter</button>
+          </div>
+          
+          
+          
+          {suggestions.length>0 && (
+            <ul className='suggestions'>
+              {suggestions.map((suggestion,index)=>(
+                <li key={index} onClick={() => {
+                  setWord(suggestion);
+                  console.log(suggestion,word)
+                  setSuggestions([]);
+                  fetchWord(suggestion)
+                }}>
+                  {suggestion}
+                </li>
+              ))}
+            </ul>
+          )}
           
           {error && <p style={{ color: "red" }}>{error}</p>}
 
@@ -59,10 +99,10 @@ const Dictionary = () => {
                   <p className='definition'>{meaning.definitions[0]?.definition || "N/A"}</p>
                   <p className='example'>{meaning?.definitions[0]?.example || "N/A"}</p>
                   <p className='synonyms'>
-                    Synonyms: {meaning?.definitions[0]?.synonyms.length > 0 ? data.meanings[0]?.definitions[0]?.synonyms.join(", ") : "None"}
+                    Synonyms: {meaning?.definitions[0]?.synonyms.length > 0 ? meaning?.definitions[0]?.synonyms.join(", ") : "None"}
                   </p>
                   <p className='antonyms'>
-                    Antonyms: {meaning?.definitions[0]?.antonyms.length > 0 ? data.meanings[0]?.definitions[0]?.antonyms.join(", ") : "None"}
+                    Antonyms: {meaning?.definitions[0]?.antonyms.length > 0 ? meaning?.definitions[0]?.antonyms.join(", ") : "None"}
                   </p>
                 </div>
               ))}
